963dd7cb39ce661c83547795f668e83c55236653
RobertoPelonara <robertopelonara@outlook.it>
2017-10-26 09:14:21 +0200

    Initial commit

---
+++ LearnCBL/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  LearnCBL
+//
+//  Created by Roberto Pelonara on 26/10/2017.
+//  Copyright © 2017 Roberto Pelonara. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
